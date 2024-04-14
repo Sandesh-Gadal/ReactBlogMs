d1855d4683d04c7d57e6af9f22e91897dccf7321
Sandesh Gadal <gadalsandesh123@gmail.com>
2024-04-14 20:23:53 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/blogs/Home'
+import Register from './pages/auth/Register'
+import AddBlog from './pages/blogs/AddBlog'
+import Login from './pages/auth/Login'
+import EditBlog from './pages/blogs/EditBlog'
+import SingleBlog from './pages/blogs/SingleBlog'
+
+function App() {
+ 
+
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/register' element={<Register/>}/>
+       <Route path='/blog/add' element={<AddBlog/>}/>
+       <Route path='/login' element={<Login/>}/>
+       <Route path='/blog/edit' element={<EditBlog/>}/>
+       <Route path='/blog/id' element={<SingleBlog/>}/> 
+  </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/blogs/EditBlog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+import Form from './components/form/Form'
+
+const EditBlog = () => {
+  return (
+   <Layout>
+      <Form  type='Edit'/>
+   </Layout>
+
+  )
+}
+
+export default EditBlog
